89b591e23e9337c6b164da8a6fbe1419eb4e1b56
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-04-20 01:47:45 +0100

    Resolving checker error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({ status: 200, body: 'success' }),
+    .then(() => ({ status: 200, body: 'Success' }),
       () => Error())
     .finally(() => console.log('Got a response from the API'));
 }
