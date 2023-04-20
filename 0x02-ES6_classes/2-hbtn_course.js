5b6d219c9d5eb97cc58f7245c35aaa112e33adb8
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-04-21 00:52:04 +0100

    Resolved eslint error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
     this.name = name;
@@ -8,27 +9,30 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(val) {
-    if (typeof val != 'string') throw new TypeError('Name must be a string');
+    if (typeof val !== 'string') throw new TypeError('Name must be a string');
     this._name = val;
   }
 
   get length() {
     return this._length;
   }
+
   set length(val) {
-    if (typeof val != 'number') throw new TypeError('Length must be a number');
+    if (typeof val !== 'number') throw new TypeError('Length must be a number');
     this._length = val;
   }
 
   get students() {
     return this._students;
   }
+
   set students(val) {
-    let error = new TypeError('Students must be an Array');
+    const error = new TypeError('Students must be an Array');
     if (!Array.isArray(val)) throw error;
     val.forEach((name) => {
-      if (typeof name != 'string') throw error;
+      if (typeof name !== 'string') throw error;
     });
 
     this._students = val;
